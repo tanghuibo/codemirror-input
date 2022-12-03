f4916d670cd57a6dbaa11ffbef67f5d369c141d3
tanghuibo <1215893264@qq.com>
2022-12-04 00:38:24 +0800

    完善事例

--- src/components/CodemirrorInput/input.vue
+++ src/components/CodemirrorInput/input.vue
@@ -5,16 +5,38 @@
 <script>
 import { EditorView } from "codemirror"
 import { EditorState } from "@codemirror/state"
+import { ViewPlugin } from "@codemirror/view"
 export default {
     name: "cm-input",
     mounted() {
-        new EditorView({
+        const editorView = new EditorView({
+            doc: "test",
             extensions: [
+                ViewPlugin.fromClass(class {
+                    update(update) {
+                        if (update.docChanged) {
+                            console.log("new Value: {}", update.state.doc.toString());
+                        } else {
+                            console.log(update)
+                        }
+                    }
+                }),
                 EditorState.transactionFilter.of(tr => {
                     return tr.newDoc.lines > 1 ? [] : [tr]
                 })],
             parent: this.$refs.cmInput
-        })
+        });
+        window.xx = editorView;
+        setTimeout(() => {
+            editorView.dispatch({
+                changes: {
+                    from: editorView.state.selection.ranges[0].from,
+                    to: editorView.state.selection.ranges[0].to,
+                    insert: "测试"
+                }
+            })
+        }, 3000);
     }
+
 }
 </script>
